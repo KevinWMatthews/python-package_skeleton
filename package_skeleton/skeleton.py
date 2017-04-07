8bd4c2b376e2b232b8698905ecb5f535da3c3389
Kevin Matthews <kmatthews@cyberdata.net>
2017-04-07 09:11:02 -0700

    Initial commit
    Basic skeleton of our skeleton package.

---
+++ package_skeleton/skeleton.py
@@ -0,0 +1,7 @@
+class Skeleton():
+    @classmethod
+    def is_alive(cls):
+        return False
+
+    def rattle(self):
+        pass
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='package_skeleton',
+      version='0.1',
+      description='An example of configuring a new Python package',
+      author='Kevin W Matthews',
+      author_email='KevinWMatthews@gmail.com',
+      license='MIT',
+      packages=['package_skeleton'],
+      zip_safe=False)
