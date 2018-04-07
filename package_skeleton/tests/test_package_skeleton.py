99dd80e491bb6e43f86380299aaff6b474244d8a
Kevin Matthews <kmatthews@cyberdata.net>
2018-04-06 17:52:51 -0700

    Tweak tests

--- package_skeleton/tests/test_package_skeleton.py
+++ package_skeleton/tests/test_package_skeleton.py
@@ -4,16 +4,21 @@ from package_skeleton import Skeleton
 def test_success():
     assert True
 
-def test_create_class_from_package():
-    Skeleton()
-
-def test_if_it_lives():
-    assert not Skeleton.is_alive()
-
-def test_rattle_skeleton():
-    skeleton = Skeleton()
-    assert 'Rattle rattle!' == skeleton.rattle()
-
 @pytest.mark.skip(reason="This test always fails")
-def test_fail():
+def test_skip_this():
     assert False
+
+class TestExamples(object):
+    def test_group_tests_in_a_class(self):
+        assert not False
+
+class TestSkeleton(object):
+    def test_create_class_from_package(self):
+        Skeleton()
+
+    def test_is_not_alive(self):
+        assert not Skeleton.is_alive()
+
+    def test_rattle_skeleton(self):
+        skeleton = Skeleton()
+        assert 'Rattle rattle!' == skeleton.rattle()
