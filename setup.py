e5c34958d2fe207515a94e6adcb8d9ef86a4b87a
Kevin Matthews <kmatthews@cyberdata.net>
2018-04-09 10:43:28 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,34 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='package_skeleton',
-      version='0.1',
-      description='An example of configuring a new Python package',
-      author='Kevin W Matthews',
-      author_email='KevinWMatthews@gmail.com',
-      license='MIT',
-      packages=['package_skeleton'],
-      zip_safe=False,
-      scripts=['bin/skeleton-rattle'],
-      include_package_data=True
+setup(
+    name='package_skeleton',
+    description='An example of configuring a new Python package',
+    version='0.1',
+    packages=find_packages(exclude=["tests"]),      # Automatically recurse this package and determine what to install
+    # install_requires=[],          # Declare dependencies here
+    python_requires='>=3.4.3',      # Specify minimum python version
+    include_package_data=True,      # Include any files specified by MANIFEST.in
+    zip_safe=False,                 # Can this project be installed and run from a zip file? Why?
+    # Auto-generate platform-specific scripts that run important features of the package
+    entry_points={
+        'console_scripts': [
+            # 'skeleton-autorattle = package_skeleton.<module>:<function>',
+        ],
+        'gui_scripts': [
+        ],
+    },
+    scripts=['bin/skeleton-rattle'],                # Custom scripts
+
+    # Metadata
+    url='https://github.com/KevinWMatthews/python-package_skeleton',
+    author='Kevin W Matthews',
+    author_email='KevinWMatthews@gmail.com',
+    license='MIT',
+
+    #TODO
+    # test_suit='',
+    # tests_require=[],
+    # test_loader=,
+
+    #TODO entry_points={},
 )
